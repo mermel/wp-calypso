b549c881d6a414875c635a16904c50c304fcb9f4
Miklos Juhasz <miklos.juhasz@automattic.com>
2016-07-07 13:24:17 +0200

    Disable push notifications in Chrome 49 and below.

--- client/state/push-notifications/utils.js
+++ client/state/push-notifications/utils.js
@@ -5,6 +5,19 @@ import {
 	isServiceWorkerSupported,
 } from 'lib/service-worker';
 
+export function isUnsupportedChromeVersion() {
+	if ( window && window.chrome && window.navigator.appVersion ) {
+		if ( getChromeVersion() < 50 ) {
+			return true;
+		}
+	}
+	return false;
+}
+
+export function getChromeVersion() {
+	return window.navigator.appVersion.match( /Chrome\/(\d+)/ )[ 1 ];
+}
+
 export function isPushNotificationsSupported() {
 	return (
 		isServiceWorkerSupported() &&
